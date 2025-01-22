b3ecf053b371f55c1433d9d703ec928571b0a07b
Jacqueline Fung <js3fung@uwaterloo.ca>
2025-01-22 11:39:19 -0500

    feat: add a working logger sink to kickstart the project (#1)
    Signed-off-by: jacque1ine <js3fung@uwaterloo.ca>
    Co-authored-by: Keran Yang <yangkr920208@gmail.com>

---
+++ src/main/java/org/example/SimpleSink.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import io.numaproj.numaflow.sinker.Datum;
+import io.numaproj.numaflow.sinker.DatumIterator;
+import io.numaproj.numaflow.sinker.Response;
+import io.numaproj.numaflow.sinker.ResponseList;
+import io.numaproj.numaflow.sinker.Server;
+import io.numaproj.numaflow.sinker.Sinker;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * This is a simple User Defined Sink example which logs the input message
+ */
+
+@Slf4j
+public class SimpleSink extends Sinker {
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(new SimpleSink());
+
+        // Start the server
+        server.start();
+
+        // wait for the server to shut down
+        server.awaitTermination();
+    }
+
+    @Override
+    public ResponseList processMessages(DatumIterator datumIterator) {
+        ResponseList.ResponseListBuilder responseListBuilder = ResponseList.newBuilder();
+        while (true) {
+            Datum datum = null;
+            try {
+                datum = datumIterator.next();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                continue;
+            }
+            // null means the iterator is closed, so we break the loop
+            if (datum == null) {
+                break;
+            }
+            try {
+                String msg = new String(datum.getValue());
+                log.info("Received message: {}, headers - {}", msg, datum.getHeaders());
+                responseListBuilder.addResponse(Response.responseOK(datum.getId()));
+            } catch (Exception e) {
+                responseListBuilder.addResponse(Response.responseFailure(
+                        datum.getId(),
+                        e.getMessage()));
+            }
+        }
+        return responseListBuilder.build();
+    }
+}
