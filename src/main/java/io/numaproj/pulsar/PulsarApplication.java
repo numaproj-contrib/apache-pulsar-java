e07552cec795cfc0f77b982421da7704e0c4114f
jacque1ine <js3fung@uwaterloo.ca>
2025-01-27 21:28:51 -0500

    rename application
    Signed-off-by: jacque1ine <js3fung@uwaterloo.ca>

--- src/main/java/io/numaproj/pulsar/PulsarApplication.java
+++ src/main/java/io/numaproj/pulsar/PulsarApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyApplication {
+public class PulsarApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(PulsarApplication.class, args);
     }
 }
