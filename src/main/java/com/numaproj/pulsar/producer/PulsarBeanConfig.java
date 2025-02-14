5dcfa80b933158b981617e49df9346cb0f0451d1
jacque1ine <js3fung@uwaterloo.ca>
2025-02-14 12:17:06 -0500

    conditional bean
    Signed-off-by: jacque1ine <js3fung@uwaterloo.ca>

--- src/main/java/com/numaproj/pulsar/producer/PulsarBeanConfig.java
+++ src/main/java/com/numaproj/pulsar/producer/PulsarBeanConfig.java
@@ -1,41 +1,59 @@
 package com.numaproj.pulsar.producer;
 
 import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.api.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.numaproj.pulsar.config.PulsarClientProperties;
 import com.numaproj.pulsar.config.PulsarProducerProperties;
 
-
 /***
  *
- * For each @Configuration annotated class it finds, Spring will create an instance of that configuration class
+ * For each @Configuration annotated class it finds, Spring will create an
+ * instance of that configuration class
  * and then call the methods marked with @Bean to get the instances of beans.
- * The objects returned by these methods are then registered within the Spring application context.
+ * The objects returned by these methods are then registered within the Spring
+ * application context.
  */
-
 @Configuration
-//TO DO: make this a conditional
 public class PulsarBeanConfig {
+
+    @Autowired
+    private PulsarClientProperties pulsarClientProperties;
+
     @Bean
-    public PulsarClient pulsarClient(PulsarClientProperties pulsarClientProperties) throws Exception {
+    public PulsarClient pulsarClient() throws PulsarClientException {
         return PulsarClient.builder()
                 .loadConf(pulsarClientProperties.getClientConfig())
                 .build();
     }
 
-    @Bean
-    public Producer<String> pulsarProducer(PulsarClient pulsarClient, PulsarProducerProperties pulsarProducerProperties) throws Exception {
-        return pulsarClient.newProducer(Schema.STRING)
-                .loadConf(pulsarProducerProperties.getProducerConfig())
-                .create();
-    }
+    // TO DO: Extend the conditional logic when implementing sink
+    @Configuration
+    @ConditionalOnProperty(name = "spring.pulsar.producer.enable", havingValue = "true", matchIfMissing = true)
+    static class ProducerConfig {
 
-    @Bean
-    public EventPublisher eventPublisher(Producer<String> producer) {
-        return new EventPublisher(producer);
+        @Autowired
+        private PulsarProducerProperties pulsarProducerProperties;
+
+        @Autowired
+        private PulsarClient pulsarClient;
+
+        @Bean
+        public Producer<String> pulsarProducer() throws PulsarClientException {
+            return pulsarClient.newProducer(Schema.STRING)
+                    .loadConf(pulsarProducerProperties.getProducerConfig())
+                    .create();
+        }
+
+        @Bean
+        public EventPublisher eventPublisher(@Autowired Producer<String> pulsarProducer) {
+            return new EventPublisher(pulsarProducer);
+        }
     }
 }
