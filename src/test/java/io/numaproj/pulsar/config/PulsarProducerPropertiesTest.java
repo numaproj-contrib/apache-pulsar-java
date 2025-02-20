80b7d6888a6dcc228f817c95aebaeb5d1c5408b7
jacque1ine <js3fung@uwaterloo.ca>
2025-02-20 10:55:58 -0500

    pulsar sink test
    Signed-off-by: jacque1ine <js3fung@uwaterloo.ca>

--- src/test/java/io/numaproj/pulsar/config/PulsarProducerPropertiesTest.java
+++ src/test/java/io/numaproj/pulsar/config/PulsarProducerPropertiesTest.java
@@ -1,4 +1,5 @@
 package io.numaproj.pulsar.config;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,30 +8,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PulsarProducerPropertiesTest {
-    
+
     private PulsarProducerProperties MakePulsarProducerProperties() {
         return new PulsarProducerProperties();
     }
 
-    // Test to check that the producerConfig map is initialized as an empty map by default when a new instance of PulsarClientProperties is created.
-    @Test  
+    // Test to check that the producerConfig map is initialized as an empty map by
+    // default when a new instance of PulsarClientProperties is created.
+    @Test
     public void ProducerConfig_DefaultInitialization_EmptyMap() {
         PulsarProducerProperties properties = MakePulsarProducerProperties();
-        
+
         Map<String, Object> result = properties.getProducerConfig();
-        
+
         assertTrue(result.isEmpty());
     }
-    
-    // Test to verify that the producerConfig map can be modified through the setter method.
-    @Test  
-    public void ProducerConfig_Modifiable_AfterInitialization() {
+
+    // Test to verify that the producerConfig map can be modified through the setter
+    // method.
+    @Test
+    public void ProducerConfig_Setter_ModifiesMap() {
         PulsarProducerProperties properties = MakePulsarProducerProperties();
-        
+
         Map<String, Object> newConfig = new HashMap<>();
         newConfig.put("key", "value");
         properties.setProducerConfig(newConfig);
-        
+
         Map<String, Object> result = properties.getProducerConfig();
         assertEquals("value", result.get("key"));
     }
