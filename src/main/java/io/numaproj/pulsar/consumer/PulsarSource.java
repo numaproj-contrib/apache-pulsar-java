11f508d2ce120243378941a832bb5543e092e0a2
Cece Ma <mayuqing131@gmail.com>
2026-04-10 09:36:04 -0400

    rm batching acking to see decline
    Signed-off-by: Cece Ma <mayuqing131@gmail.com>

--- src/main/java/io/numaproj/pulsar/consumer/PulsarSource.java
+++ src/main/java/io/numaproj/pulsar/consumer/PulsarSource.java
@@ -12,7 +12,6 @@ import io.numaproj.pulsar.config.consumer.PulsarConsumerProperties;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.pulsar.client.api.Consumer;
-import org.apache.pulsar.client.api.MessageId;
 import org.apache.pulsar.client.api.Messages;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.api.SchemaSerializationException;
@@ -184,18 +183,17 @@ public class PulsarSource extends Sourcer {
             return;
         }
 
-        // because request key values and messsages to ack key values already match, can directly iterate over the messagesToAck map values to get the message ids
-        List<MessageId> messageIds = messagesToAck.values().stream()
-                .map(org.apache.pulsar.client.api.Message::getMessageId)
-                .toList();
-
+        // TEMPORARY: single-ack each message individually to measure performance impact vs batch ack
         try {
-            if (pulsarConsumerProperties.isUseAutoConsumeSchema()) {
-                pulsarConsumerManager.getOrCreateGenericRecordConsumer(0, 0).acknowledge(messageIds);
-            } else {
-                pulsarConsumerManager.getOrCreateBytesConsumer(0, 0).acknowledge(messageIds);
+            Consumer<?> consumer = pulsarConsumerProperties.isUseAutoConsumeSchema()
+                    ? pulsarConsumerManager.getOrCreateGenericRecordConsumer(0, 0)
+                    : pulsarConsumerManager.getOrCreateBytesConsumer(0, 0);
+            int count = 0;
+            for (org.apache.pulsar.client.api.Message<?> msg : messagesToAck.values()) {
+                consumer.acknowledge(msg.getMessageId());
+                count++;
             }
-            log.info("Successfully acknowledged {} messages", messageIds.size());
+            log.info("Successfully acknowledged {} messages (single-ack mode)", count);
         } catch (PulsarClientException e) {
             log.error("Failed to acknowledge Pulsar messages", e);
         }
