fd789facdf651f66a219619aab8956a1ff56f603
github-action-benchmark <github@users.noreply.github.com>
2026-04-15 16:48:51 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 7330b3c3a57ec81186349a801ec7e93198f7815c

---
+++ dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1776271731414,
+  "repoUrl": "https://github.com/numaproj-contrib/apache-pulsar-java",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "mayuqing131@gmail.com",
+            "name": "ccm32004",
+            "username": "ccm32004"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7330b3c3a57ec81186349a801ec7e93198f7815c",
+          "message": "feat: benchmarking cicd (#84)\n\nSigned-off-by: Cece Ma <mayuqing131@gmail.com>",
+          "timestamp": "2026-04-15T12:32:40-04:00",
+          "tree_id": "70fcfbcec8d3b41b76a620b8c818828ae65963c8",
+          "url": "https://github.com/numaproj-contrib/apache-pulsar-java/commit/7330b3c3a57ec81186349a801ec7e93198f7815c"
+        },
+        "date": 1776271730969,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Consumer Throughput",
+            "value": 32423.08,
+            "unit": "msgs/sec"
+          },
+          {
+            "name": "Processing Latency (per batch)",
+            "value": 15.26,
+            "unit": "ms"
+          },
+          {
+            "name": "Read Latency (per batch)",
+            "value": 9.09,
+            "unit": "ms"
+          },
+          {
+            "name": "Ack Latency (per batch)",
+            "value": 0.54,
+            "unit": "ms"
+          },
+          {
+            "name": "Consumer CPU",
+            "value": 1330,
+            "unit": "millicores"
+          },
+          {
+            "name": "Consumer Memory",
+            "value": 361,
+            "unit": "MiB"
+          }
+        ]
+      }
+    ]
+  }
+}
