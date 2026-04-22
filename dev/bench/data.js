d6e2d1ae31b7ae2dd82bb5adf5fc7a27390aa26b
github-action-benchmark <github@users.noreply.github.com>
2026-04-22 14:42:44 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 11f46e753fa38553942274006f2a2a1995023bc0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776358492149,
+  "lastUpdate": 1776868964870,
   "repoUrl": "https://github.com/numaproj-contrib/apache-pulsar-java",
   "entries": {
     "Benchmark": [
@@ -107,6 +107,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Consumer Memory",
             "value": 328,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mayuqing131@gmail.com",
+            "name": "ccm32004",
+            "username": "ccm32004"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "11f46e753fa38553942274006f2a2a1995023bc0",
+          "message": "feat: structured logging (#86)\n\nSigned-off-by: Cece Ma <mayuqing131@gmail.com>",
+          "timestamp": "2026-04-22T10:26:15-04:00",
+          "tree_id": "1770e902b6db734cf559bce3954a11bb4d262d48",
+          "url": "https://github.com/numaproj-contrib/apache-pulsar-java/commit/11f46e753fa38553942274006f2a2a1995023bc0"
+        },
+        "date": 1776868963905,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Consumer Throughput",
+            "value": 33833.33,
+            "unit": "msgs/sec"
+          },
+          {
+            "name": "Processing Latency (per batch)",
+            "value": 14.79,
+            "unit": "ms"
+          },
+          {
+            "name": "Read Latency (per batch)",
+            "value": 7.34,
+            "unit": "ms"
+          },
+          {
+            "name": "Ack Latency (per batch)",
+            "value": 0.54,
+            "unit": "ms"
+          },
+          {
+            "name": "Consumer CPU",
+            "value": 1323,
+            "unit": "millicores"
+          },
+          {
+            "name": "Consumer Memory",
+            "value": 339,
             "unit": "MiB"
           }
         ]
